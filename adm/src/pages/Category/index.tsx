18f9dc767465228e7a5a959a3b3ffa87346a10f7
Carolaine Alexandre <115526459+CarolaineAlexandre@users.noreply.github.com>
2023-10-26 18:21:00 -0300

    Created Page Category

---
+++ adm/src/pages/Category/index.tsx
@@ -0,0 +1,13 @@
+import ProductList from "../../components/Category_grid";
+import LayoutProps from "../../components/Layout";
+
+
+export default function Home(){
+    return (
+        <>
+        <LayoutProps>
+            <ProductList/>
+        </LayoutProps>
+        </>
+    )
+    }
