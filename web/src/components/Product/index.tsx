9532cb0ac26bd2bf4f333324a7304fa9f11633ee
Carolaine Alexandre <115526459+CarolaineAlexandre@users.noreply.github.com>
2023-11-04 19:34:30 -0300

    4.0.0 | Update Api Product

--- web/src/components/Product/index.tsx
+++ web/src/components/Product/index.tsx
@@ -1,35 +1,12 @@
 
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import ProductDetail, { Product } from '../ProductDetail';
-
-//  API
-const API_URL = 'https://api-catalogo-pi.onrender.com';
+import ProductDetail from '../ProductDetail'
 
 // obtém e exibe os detalhes do produto
-const ProductDetailPage: React.FC = () => {
-  const [product, setProduct] = useState<Product | null>(null);
-
-  useEffect(() => {
-    // faz uma solicitação GET para obter os detalhes do produto
-    axios
-      .get(`${API_URL}/product/${product}`) // Substitui 'productId' pelo ID do produto desejado
-      .then((response) => {
-        // atualiza o estado com os dados do produto recebidos da API
-        setProduct(response.data);
-      })
-      .catch((error) => {
-        console.error('Erro ao obter detalhes do produto:', error);
-      });
-  }, []); 
-
+function ProductDetailPage(){
+  
   return (
     <div>
-      {product ? (
-        <ProductDetail product={product} />
-      ) : (
-        <p>Carregando...</p>
-      )}
+      <ProductDetail></ProductDetail>
     </div>
   );
 };
