c76f6a552aa9e11e9389aadc519747dc09c94703
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-11-04 13:29:35 -0300

    3.4.0 | Fixed errors and settings hooks and config axios

--- adm/src/hooks/queries/tarefas/index.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery} from '@tanstack/react-query'
-import {api} from  '../../../helpers/axios'
-
-// export const useTarefas = () => {
-//     return useQuery({
-//         queryKey: ['tarefas'],
-//         queryFn: () => api.get('/task').then((res)=>res.data)
-//     })
-// }
