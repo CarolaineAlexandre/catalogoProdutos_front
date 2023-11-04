1d0a5e70dcc880d111e165dcae71fc6495d3b270
GabrielNeimester <115425284+GabrielNeimester@users.noreply.github.com>
2023-11-04 15:01:38 -0300

    fixed Footer

--- adm/src/components/Layout/index.tsx
+++ adm/src/components/Layout/index.tsx
@@ -10,7 +10,7 @@ export default function LayoutProps(props: Props){
     const {children} = props
     return(
         <>
-    <Grid  className="grid" templateColumns='1fr' templateRows='auto auto 64px' height="100vh" bg={'rgba(249, 249, 249, 1)'} overflow={'hidden'}>
+    <Grid  className="grid" templateColumns='1fr' templateRows='auto auto 64px' minHeight="100vh" bg={'rgba(249, 249, 249, 1)'} overflow={'hidden'}>
     <GridItem colSpan={1} rowSpan={1}>
         <NavBarProps/>
     </GridItem>
