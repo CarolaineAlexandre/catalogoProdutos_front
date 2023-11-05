6b3c4e6679b6a4910ffdf52de8a190faf47a8a57
Carolaine Alexandre <115526459+CarolaineAlexandre@users.noreply.github.com>
2023-11-05 16:18:40 -0300

    4.0.1 | Update page DetailProduct

--- web/src/pages/DetailProduct/index.tsx
+++ web/src/pages/DetailProduct/index.tsx
@@ -1,71 +1,45 @@
-import {
-  Container,
-  Box,
-  Button,
-  Text,
-  Heading,
-  VStack,
-  useColorModeValue,
-} from '@chakra-ui/react';
-import LayoutProps from '../../components/Layout';
+import { useProduct } from '../../hooks/queries/product';
+import { Card, Center, Grid, Heading, Image, Text } from '@chakra-ui/react';
 
-export default function Simple() {
-  const product = {
-    name: 'Vela',
-    price: 350.0,
-    description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr...',
-  };
+export interface IProduct {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  category: string;
+  photo: string;
+  color: string;
+  promotion: boolean;
+}
+
+function ProductDetail() {
+  const {
+    data: productList,
+    refetch: refetchProduct
+  } = useProduct()
+  console.log(productList)
+
+  const product = productList
 
   return (
-    <LayoutProps>
-    <Container maxW={'7xl'}>
-      <VStack
-        spacing={4}
-        alignItems="start"
-        flexDirection={{ base: 'column', lg: 'row' }}
-      >
-        <Box width={{ base: '100%', lg: '40%' }}>
-          <img
-            src={
-              'https://th.bing.com/th/id/R.8a23c29ac48bd82b132ddac728e24449?rik=dmWJK7rpdMKm9w&riu=http%3a%2f%2fnatalia.blog.br%2fwp-content%2fuploads%2f2014%2f12%2fVelas-decorativas-de-Natal-17.jpg&ehk=EWYfVlXbmqjQtAB07X%2bRWPFH%2fGX8M3bDFroY0vOtWZE%3d&risl=&pid=ImgRaw&r=0'
-            }
-            alt="product image"
-            style={{
-              maxWidth: '60%',
-              height: 'auto',
-            }}
-          />
-        </Box>
-        <VStack spacing={4} alignItems="start" width={{ base: '100%', lg: '60%' }}>
-          <Heading
-            lineHeight={1.1}
-            fontWeight={600}
-            fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}
-          >
-            {product.name}
-          </Heading>
-          <Text
-            color={useColorModeValue('gray.900', 'gray.400')}
-            fontWeight={300}
-            fontSize="2xl"
-          >
-            R${product.price.toFixed(2)} 
-          </Text>
-          <Text
-            color={useColorModeValue('gray.500', 'gray.400')}
-            fontSize="xl"
-          >
-            {product.description}
-          </Text>
-          <Button 
-          background="#7A5656" 
-          color="white" 
-          _hover={{ bg: '#c5904A' }}
-          >
-            Tenho Interesse</Button>
-        </VStack>
-      </VStack>
-    </Container>
-    </LayoutProps>
+
+    <div>
+      {productList && (
+      <Center>
+        <Grid >
+          <Card padding={'120px'}>
+            <Heading>{product.name}</Heading>
+            <Image src={product.photo} alt={product.name} />
+            <Text>Cor: {product.color}</Text>
+            <Text>Preço: R${product.price}</Text>
+            <Text>Descrição: {product.description}</Text>
+            {product.promotion && <Text>Está na promoção!</Text>}
+          </Card>
+        </Grid>
+      </Center>
+      )}
+    </div>
   );
-}
+};
+export default ProductDetail;
+
