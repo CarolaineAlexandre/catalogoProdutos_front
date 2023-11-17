fbca0280eab0e96f18b2bbd0ea4cbdd8066f4ec6
GabrielNeimester <115425284+GabrielNeimester@users.noreply.github.com>
2023-11-17 16:47:32 -0300

    Delete adm directory

--- adm/src/hooks/queries/category/index.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery} from '@tanstack/react-query'
-import {api} from  '../../../helpers/axios'
-
-export const useCategories = () => {
-    return useQuery({
-        queryKey: ['categories'],
-        queryFn: () => api.get('/category').then((res)=>res.data)
-    })
-}
