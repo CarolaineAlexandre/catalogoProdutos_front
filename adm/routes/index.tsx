7a0f6107abc8aecf6e76cc14e70b93a9563ca30a
Barbara Bruzon <105392656+barbara-bruzon@users.noreply.github.com>
2023-11-19 14:42:53 -0300

    feat: add routes to pages related to categories

--- adm/routes/index.tsx
+++ adm/routes/index.tsx
@@ -5,17 +5,20 @@ import Home from "../src/pages/Home";
 import ProductPage from "../src/pages/Products";
 
 import CategoryPage from "../src/pages/ProductList";
-import EditCategory from "../src/pages/EditCategory";
 import EditProductPage from "../src/pages/EditProduct";
-
+import Category from "../src/pages/Category";
+import ListCategory from "../src/pages/ListCategory";
+import EditCategory from "../src/pages/EditCategory";
 
 const router = createBrowserRouter([
     {path: '/home', element: <Home/>, errorElement: <NotFoundPage />},
     {path: '/', element: <Login/>, errorElement: <NotFoundPage />},
     {path: '/produtos', element: <ProductPage/>},
     {path: '/listarprodutos', element: <CategoryPage/>},
-    {path: '/categorias', element: <EditCategory/>},
     {path: '/editarprodutos/:id', element: <EditProductPage/>},
+    {path: '/categorias', element: <Category/>},
+    {path: '/listarcategorias', element: <ListCategory/>},
+    {path: '/editarcategorias/:id', element: <EditCategory />},
     {path: '/error', element: <NotFoundPage/>, errorElement: <NotFoundPage/>}
 
   ])
