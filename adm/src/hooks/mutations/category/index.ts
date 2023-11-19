ddc4b52422c6dc296ede64b898ff742f04fe4d15
Barbara Bruzon <105392656+barbara-bruzon@users.noreply.github.com>
2023-11-19 14:48:10 -0300

    feat: create mutation file for categories

---
+++ adm/src/hooks/mutations/category/index.ts
@@ -0,0 +1,17 @@
+import { useMutation } from "@tanstack/react-query"
+import { api } from "../../../helpers/axios"
+
+interface ICategory {
+    name: string
+    description: string
+    photo?:string
+}
+
+const postCategory = async (data:ICategory) => {
+    await api.post('/category', data)
+    console.log(data)
+}
+
+export const useCreateCategory = () => useMutation({
+    mutationFn: (data: ICategory) => postCategory(data)
+})
