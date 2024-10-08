c91cf004d8924418a865983ff95844cccd47e391
devGiovanni95 <giovanni.devjava@hotmail.com>
2024-10-08 12:49:42 -0300

    5.7.0 | Add dockerfile and update layout

--- adm/src/components/Layout/index.tsx
+++ adm/src/components/Layout/index.tsx
@@ -10,16 +10,27 @@ export default function LayoutProps(props: Props){
     const {children} = props
     return(
         <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-        <Grid  className="grid" templateColumns='repeat(6, 1fr)' templateRows='auto'  flex="1" width="100%" height="100vh" bg={'rgba(249, 249, 249, 1)'} overflow={'hidden'} overflowY="auto">
-        <GridItem colSpan={6} >
-            <NavBarProps/>
-        </GridItem>
-        <GridItem colSpan={6} >
-            {children}
-        </GridItem>
-        <GridItem colSpan={6} mt={6}>
-            <FooterProps/>
-        </GridItem>
+        <Grid  
+            className="grid" 
+            templateColumns='repeat(6, 1fr)' 
+            // templateRows='auto'  
+            templateRows="auto 1fr auto"
+            flex="1" 
+            width="100%" 
+            // height="100vh" 
+            bg={'rgba(249, 249, 249, 1)'} 
+            // overflow={'hidden'} 
+            // overflowY="auto"
+        >
+            <GridItem colSpan={6} >
+                <NavBarProps/>
+            </GridItem>
+            <GridItem colSpan={6} overflowY={"auto"} mt={{ base: 0, md: 70 }}>
+                {children}
+            </GridItem>
+            <GridItem colSpan={6} mt={6}>
+                <FooterProps/>
+            </GridItem>
         </Grid>
     </div>
         )
